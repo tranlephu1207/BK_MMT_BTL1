8e7b24c3c76adde2b339cd00e88b7c7f59d5b4dd
Phu Tran <phu.tran@star.global>
2023-11-28 12:07:13 +0700

    feat: peer to peer FETCH DOWNLOAD

--- client1.py
+++ client1.py
@@ -1,4 +1,9 @@
 import socket
+import select
+import threading
+
+import urllib.request
+
 
 # IP = socket.gethostbyname(socket.gethostname())
 IP =  '61.28.231.242'
@@ -6,16 +11,82 @@ IP =  '61.28.231.242'
 PORT = 9896
 ADDR = (IP, PORT)
 SIZE = 1024
+BACKLOG = 10
 FORMAT = "utf-8"
 DISCONNECT_MSG = "!DISCONNECT"
 PUBLISH = 'publish'
 FETCH = 'fetch'
 SEND = 'send'
 DOWNLOAD = 'download'
+EXIT = 'exit'
+
+LISTEN_PORT = 57244
 
 local_files = {}
 
-def main():
+#function to send file
+def sendf(soc):
+    soc.send('file name: ')
+    fname = soc.recv(SIZE)    
+    file=open(fname,'rb')
+    data=file.read()
+    soc.send(data)
+    print('File sent')
+
+#function for client to connect to listening client                
+def cconnect(peer_ip, peer_port, fname, lname):
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    try:
+        soc.connect((peer_ip,peer_port))
+    except:
+        print('Unable to connect to client')
+    print('now connected')
+    while 1:
+        msg = soc.recv(SIZE)
+        if DOWNLOAD in msg:
+            sendf(soc)
+        if EXIT in msg:
+            print('session ended')
+            soc.close()
+            break
+        # user_input = raw_input('<Me> ')
+        # soc.send(user_input)
+        # if user_input == '\SEND_FILE':
+        #     recvf(soc)  
+        # if user_input == '\CLOSE_SESSION':
+        #     print 'session ended'
+        #     soc.close()
+        #     break
+    return
+
+def handle_client(conn, addr):
+    print(f"[PEER NEW CONNECTION] {addr} connected")
+    connected = True
+    while connected:
+      msg = conn.recv(SIZE).decode(FORMAT)
+      print(f"Msg: {msg}")
+      if DOWNLOAD in msg:
+        print(f"DOWNLOAD: ")
+
+def plisten():
+  external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+  print(external_ip)
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  #binding the socket to external ip and port
+  s.bind(("0.0.0.0", LISTEN_PORT))
+  s.listen()
+  print(f"listening to {external_ip}:{LISTEN_PORT}")
+
+  while True:
+    conn, addr = s.accept()
+    thread = threading.Thread(target=handle_client, args=(conn, addr))
+    thread.start()
+    print(f"[PEER ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+
+def pconnect():
   client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   client.connect(ADDR)
   print(f"[CONNECTED] Client connected to server at {IP}:{PORT}")
@@ -36,8 +107,14 @@ def main():
       print(f"[SERVER PUBLISH] {msg}")
     elif FETCH in msg:
       print('FETCH')
-      # msg = client.recv(SIZE).decode(FORMAT)
-      # print(f"[SERVER] {msg}")
+      msg = client.recv(SIZE).decode(FORMAT)
+      print(f"[SERVER] {msg}")
+      infos = msg.split()
+      fname = infos[1]
+      lname = infos[2]
+      peer_ip = infos[3]
+      peer_port = infos[4]
+      cconnect(peer_ip=peer_ip, peer_port=peer_port, fname=fname, lname=lname)
     elif SEND in msg:
       print('SEND')
       # msg = client.recv(SIZE).decode(FORMAT)
@@ -49,6 +126,13 @@ def main():
     else:
       msg = client.recv(SIZE).decode(FORMAT)
       print(f"[SERVER] {msg}")
+
+def main():
+  connect_thread = threading.Thread(target=pconnect)
+  # listen_thread = threading.Thread(target=plisten)
+
+  connect_thread.start()
+  # listen_thread.start()
 
 
 if __name__ == "__main__":
--- server1.py
+++ server1.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 import pprint
-from ping3 import ping, verbose_ping
+# from ping3 import ping, verbose_ping
 
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 9896
@@ -26,6 +26,10 @@ files = {}
 
 def handle_client(conn, addr):
   print(f"[NEW CONNECTION] {addr} connected")
+  ip = addr[0]
+  port = addr[1]
+  connection_key = f"{ip}:{port}"
+  
   connected = True
   first_login = True
   while connected:
@@ -33,11 +37,16 @@ def handle_client(conn, addr):
     # print(f"Msg: {msg}")
     if first_login:
       first_login = False
-      connections[addr[0]] = {
-        "ip": addr[0],
-        "port": addr[1],
+      connections[connection_key] = {
+        "ip": ip,
+        "port": port,
         "name": msg,
       }
+      # connections[addr[0]] = {
+      #   "ip": addr[0],
+      #   "port": addr[1],
+      #   "name": msg,
+      # }
       pprint.pprint(connections, width=1)
       msg = f"Welcome {msg}"
       conn.send(msg.encode(FORMAT))
@@ -51,19 +60,21 @@ def handle_client(conn, addr):
       print(f'PUBLISH: {msg}')
       infos = msg.split()
       pprint.pprint(infos)
+      link = infos[1]
+      if link[0] == "'" and link[-1] == "'":
+        link = link[1:-1]
+      print(f"link: {link}")
       fname = infos[2]
       if files.get(fname) != None:
         error = f"{fname} exists"
-        print(error)
-        print('abc')
         conn.send(error.encode(FORMAT))
       else:
         files[fname] = {
-          "ip": addr[0],
-          "port": addr[1],
+          "ip": ip,
+          "port": port,
           "fname": fname,
-          "lname": infos[1],
-          "name": connections.get(addr[0]).get('name')
+          "lname": link,
+          "name": connections.get(connection_key).get('name')
         }
         pprint.pprint(files, width=1)
         msg = f"{fname} added"
@@ -72,6 +83,10 @@ def handle_client(conn, addr):
       print(f'FETCH: {msg}')
       infos = msg.split()
       pprint.pprint(infos)
+      fname = infos[1]
+      file_info = files[fname]
+      msg = f"fetch {fname} {file_info.get('lname')} {file_info.get('ip')} {file_info.get('port')}"
+      conn.send(msg.encode(FORMAT))
     elif SEND in msg:
       print(f'SEND: {msg}')
     elif DOWNLOAD in msg:
@@ -83,6 +98,34 @@ def handle_client(conn, addr):
 
   conn.close()
 
+def server_terminal():
+  server_command = input()
+  print(f"server_command: {server_command}")
+  open_terminal = True
+  while open_terminal:
+    server_command = input('> ')
+    if server_command == CLOSE_TERMINAL:
+      open_terminal = False
+    elif GET_CONNECTIONS in server_command:
+      pprint.pprint(connections, width=1)
+    elif PING_HOSTNAME in server_command:
+      strs = server_command.split()
+      # verbose_ping(strs[1])
+    elif DISCOVER_HOSTNAME in server_command:
+      print('discover host name')
+      strs = server_command.split()
+      hostname = strs[1]
+      print(f"Host name: {hostname}")
+      result = {}
+      for file in files.values():
+        file_ip = file.get('ip')
+        file_port = file.get('port')
+        file_addr = f"{file_ip}:{file_port}"
+        if file_addr == hostname:
+          result[file.get('fname')] = file
+      
+      print(f'discover from ${hostname} success')
+      pprint.pprint(result)
 
 def main():
   print("[STARTING] server is starting...")
@@ -91,37 +134,39 @@ def main():
   server.listen()
   print(f"[LISTEN] Server is listening on {IP}:{PORT}")
 
+  server_terminal_thread = threading.Thread(target=server_terminal)
+  server_terminal_thread.start()
+
   while True:
     conn, addr = server.accept()
     thread = threading.Thread(target=handle_client, args=(conn, addr))
     thread.start()
-    print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
-    server_command = input()
-    print(f"server_command: {server_command}")
-    if server_command == SERVER_TERMINAL:
-      open_terminal = True
-      while open_terminal:
-        server_command = input('> ')
-        if server_command == CLOSE_TERMINAL:
-          open_terminal = False
-        elif GET_CONNECTIONS in server_command:
-          pprint.pprint(connections, width=1)
-        elif PING_HOSTNAME in server_command:
-          print('abc')
-          strs = server_command.split()
-          verbose_ping(strs[1])
-        elif DISCOVER_HOSTNAME in server_command:
-          print('discover host name')
-          strs = server_command.split()
-          hostname = strs[1]
-          print(f"Host name: {hostname}")
-          result = {}
-          for file in files.values():
-            if file.get('ip') == hostname:
-              result[file.get('fname')] = file
+    print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 2}")
+    # server_command = input()
+    # print(f"server_command: {server_command}")
+    # if server_command == SERVER_TERMINAL:
+    #   open_terminal = True
+    #   while open_terminal:
+    #     server_command = input('> ')
+    #     if server_command == CLOSE_TERMINAL:
+    #       open_terminal = False
+    #     elif GET_CONNECTIONS in server_command:
+    #       pprint.pprint(connections, width=1)
+    #     elif PING_HOSTNAME in server_command:
+    #       strs = server_command.split()
+    #       # verbose_ping(strs[1])
+    #     elif DISCOVER_HOSTNAME in server_command:
+    #       print('discover host name')
+    #       strs = server_command.split()
+    #       hostname = strs[1]
+    #       print(f"Host name: {hostname}")
+    #       result = {}
+    #       for file in files.values():
+    #         if file.get('ip') == hostname:
+    #           result[file.get('fname')] = file
           
-          print(f'discover from ${hostname} success')
-          pprint.pprint(result)
+    #       print(f'discover from ${hostname} success')
+    #       pprint.pprint(result)
 
 if __name__ == '__main__':
   main()
