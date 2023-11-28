51938e139e3dffb9170ca547af7eb480400c6180
Phu Tran <phu.tran@star.global>
2023-11-29 00:16:40 +0700

    feat: save connected sockets

--- client1.py
+++ client1.py
@@ -11,8 +11,8 @@ CLIENT_ADDR = (CLIENT_IP, CLIENT_PORT)
 
 # IP = socket.gethostbyname(socket.gethostname())
 # IP = "0.0.0.0"
-# IP = "192.168.1.12"
-IP =  '61.28.231.242'
+IP = "192.168.1.12"
+# IP =  '61.28.231.242'
 # IP = socket.gethostbyname(socket.gethostname())
 PORT = 9896
 ADDR = (IP, PORT)
@@ -29,6 +29,8 @@ EXIT = 'exit'
 LISTEN_PORT = 57244
 
 local_files = {}
+
+connected_sockets = {}
 
 #function to send file
 def sendf(soc, lname):
@@ -49,32 +51,54 @@ def recvf(soc, fname):
 
 #function for client to connect to listening client                
 def cconnect(peer_ip, peer_port, fname, lname):
+    # socket_key = f"{peer_ip}:{CLIENT_PORT}"
+    # if connected_sockets.get(socket_key):
+    #    soc = connected_sockets.get(socket_key)
+    # else:
+    #   soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #   soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    #   try:
+    #       soc.connect((peer_ip,CLIENT_PORT))
+    #   except:
+    #       print('Unable to connect to client')
+    #       return
+    #   print('now connected')
+    #   connected_sockets[socket_key] = soc
+
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     try:
-        # soc.connect((peer_ip,peer_port))
         soc.connect((peer_ip,CLIENT_PORT))
     except:
         print('Unable to connect to client')
         return
     print('now connected')
+    # connected_sockets[socket_key] = soc
     msg = f"download {fname} {lname}".encode(FORMAT)
     soc.send(msg)
     recvf(soc, fname=fname)
-    while 1:
-       data = soc.recv(SIZE).decode(FORMAT)
-       if EXIT in data:
-          soc.close()
-          break
+    soc.close()
+    # while 1:
+    #    data = soc.recv(SIZE).decode(FORMAT)
+    #    if EXIT in data:
+    #       soc.close()
+    #       del connected_sockets[socket_key]
+    #       break
 
 def handle_client(conn, addr):
     print(f"[PEER NEW CONNECTION] {addr} connected")
     connected = True
     while connected:
-      msg = conn.recv(SIZE).decode(FORMAT)
-      print(f"Msg: {msg}")
-      if DOWNLOAD in msg:
-        print(f"DOWNLOAD: ")
+      data = conn.recv(SIZE).decode(FORMAT)
+      if DOWNLOAD in data:
+        print(f"[LISTEN] Received Download request: {data}")
+        infos = data.split()
+        lname = infos[2]
+        fname = infos[1]
+        msg = f"send {fname}".encode(FORMAT)
+        sendf(conn, lname=lname)
+        conn.send(msg)
+        conn.close()
 
 def plisten():
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -86,9 +110,9 @@ def plisten():
 
   while True:
     conn, addr = s.accept()
-    thread = threading.Thread(target=handle_client, args=(conn, addr))
-    thread.start()
-    print(f"[PEER ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+    # thread = threading.Thread(target=handle_client, args=(conn, addr))
+    # thread.start()
+    # print(f"[PEER ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
     connected = True
     while connected:
@@ -101,6 +125,7 @@ def plisten():
         msg = f"send {fname}".encode(FORMAT)
         sendf(conn, lname=lname)
         conn.send(msg)
+        connected = False
        
 
 
@@ -109,6 +134,9 @@ def pconnect():
   client.connect(ADDR)
   print(f"[CONNECTED] Client connected to server at {IP}:{PORT}")
   first_login = True
+
+  # thread = threading.Thread(target=handle_client, args=(client, ADDR))
+  # thread.start()
 
   connected = True
   while connected:
@@ -133,16 +161,9 @@ def pconnect():
       peer_ip = infos[3]
       peer_port = infos[4]
       if peer_ip != CLIENT_IP:
-        cconnect_thread = threading.Thread(target=cconnect, args=(peer_ip, peer_port, fname, lname))
-        cconnect_thread.start()
-    elif SEND in msg:
-      print('SEND')
-      # msg = client.recv(SIZE).decode(FORMAT)
-      # print(f"[SERVER] {msg}")
-    elif DOWNLOAD in msg:
-      print('DOWNLOAD')
-      # msg = client.recv(SIZE).decode(FORMAT)
-      # print(f"[SERVER] {msg}")
+        # cconnect_thread = threading.Thread(target=cconnect, args=(peer_ip, peer_port, fname, lname))
+        # cconnect_thread.start()
+        cconnect(peer_ip=peer_ip, peer_port=peer_port, fname=fname, lname=lname)
     else:
       msg = client.recv(SIZE).decode(FORMAT)
       print(f"[SERVER] {msg}")
